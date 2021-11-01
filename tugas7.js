76fcbcdc109ddf833932409644a1c78985cf10d1
farhan <reyhanreynold11@gmail.com>
2021-11-02 01:05:24 +0700

    tugas 7 js

---
+++ tugas7.js
@@ -0,0 +1,15 @@
+const tinggiBadan = [1,2,3,4,5,6,7,8,9,10];
+
+//perulangan for of
+for (const tinggi of tinggiBadan){
+    console.log(tinggi);
+}
+
+console.log('==================================');
+
+//perulangan biasa
+for(let tinggiBadan = 0; tinggiBadan <= 10; tinggiBadan++){
+    console.log(tinggiBadan);
+}
+
+//perulangan for of dimulai dari isi array sedangkan for dari index array
